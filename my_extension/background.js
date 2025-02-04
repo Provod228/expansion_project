f30a8c35cad6db2fd1279e5f47e51fefa78fb72e
Provod228 <tulush.ignat@bk.ru>
2025-02-04 09:03:34 +0300

    window chat

--- my_extension/background.js
+++ my_extension/background.js
@@ -1,3 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension installed");
 });
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.windows.create({
+        url: 'http://localhost:8000/chat/',
+        type: 'popup',
+        width: 400, // Ширина окна
+        height: 600 // Высота окна
+    });
+});
--- my_extension/popup.js
+++ my_extension/popup.js
@@ -1,4 +1,1 @@
-document.getElementById('fetch-data').addEventListener('click', () => {
-    // Перенаправление на страницу чата
-    window.open('http://localhost:8000/chat/', '_blank'); // Открывает в новой вкладке
-});
+window.location.href = 'http://localhost:8000/chat/'; // Перенаправление на страницу чата
