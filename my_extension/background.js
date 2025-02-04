0e48dbb16fe14f37b71aa750f3f6ff7f71fff3e5
Provod228 <tulush.ignat@bk.ru>
2025-02-04 10:46:18 +0300

    add on off

--- my_extension/background.js
+++ my_extension/background.js
@@ -2,11 +2,22 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension installed");
 });
 
+let chatWindowId = null; // Переменная для хранения ID окна чата
+
 chrome.action.onClicked.addListener((tab) => {
-    chrome.windows.create({
-        url: 'http://localhost:8000/chat/',
-        type: 'popup',
-        width: 400, // Ширина окна
-        height: 600 // Высота окна
-    });
+    if (chatWindowId) {
+        // Если окно уже открыто, закройте его
+        chrome.windows.remove(chatWindowId);
+        chatWindowId = null; // Сбросить ID окна
+    } else {
+        // Открыть новое окно чата
+        chrome.windows.create({
+            url: 'http://localhost:8000/chat/', // URL вашего чата
+            type: 'popup',
+            width: 500, // Увеличенная ширина окна
+            height: 700 // Увеличенная высота окна
+        }, (window) => {
+            chatWindowId = window.id; // Сохранить ID нового окна
+        });
+    }
 });
